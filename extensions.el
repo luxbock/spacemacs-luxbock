e25d4496a72e837609c6a8732371337f13ae66b0
luxbock <opieppo@gmail.com>
2015-12-08 19:26:13 +0700

    Getting ready to throw things into Stow

--- extensions.el
+++ extensions.el
@@ -1,11 +1,8 @@
-(defvar luxbock-post-extensions
+(setq luxbock-post-extensions
   '(
-    leuven-dark-theme
     material-theme
-    evil-paxedit
-    align-let
     org-projectile
-    evil-cleverparens
+    dactyl-mode
     ))
 
 (defun luxbock/init-leuven-dark-theme ()
@@ -16,7 +13,7 @@
 
 (defun luxbock/init-align-let ()
   (require 'align-let)
-  (evil-leader/set-key-for-mode 'emacs-lisp-mode "ma" 'align-let))
+  (spacemacs/set-leader-keys 'emacs-lisp-mode "ma" 'align-let))
 
 (defun luxbock/init-org-projectile ()
   (require 'org-projectile)
@@ -30,17 +27,12 @@
                       luxbock/org-projectile-todo-template)))
       (org-projectile:capture-for-current-project template)))
 
-  (evil-leader/set-key
+  (spacemacs/set-leader-keys
     "op" 'luxbock/org-projectile-capture-for-current-project))
 
-(defun luxbock/init-evil-cleverparens ()
-  (setq evil-cleverparens-swap-move-by-word-and-symbol t)
-  (require 'evil-cleverparens)
-  (add-hook 'emacs-lisp-mode-hook #'evil-cleverparens-mode)
-  (add-hook 'cider-mode-hook #'evil-cleverparens-mode)
-  (add-hook 'cider-repl-mode-hook #'evil-cleverparens-mode)
-  (add-hook 'cider-clojure-interaction-mode-hook #'evil-cleverparens-mode))
-
 (defun luxbock/init-material-theme ()
-  (add-to-list 'custom-theme-load-path "~/spacemacs-luxbock/extensions/emacs-material-theme")
-  )
+  (add-to-list 'custom-theme-load-path "~/spacemacs-luxbock/extensions/emacs-material-theme"))
+
+(defun luxbock/init-dactyl-mode ()
+  (use-package dactyl-mode
+    :mode ("\\pentadactylrc$" . dactyl-mode)))
--- keybindings.el
+++ keybindings.el
@@ -2,69 +2,41 @@
 
 (spacemacs/declare-prefix "y" "yasnippet")
 
-(evil-leader/set-key
-  ;; Preserve my own window-layout
-  "w." 'spacemacs/window-manipulation-micro-state
-  "ww" 'helm-mini
-  "w2" 'layout-double-columns
-  "w3" 'layout-triple-columns
-  "wc" 'evil-window-new
-  "wD" 'kill-buffer-and-window
+(spacemacs/set-leader-keys
   "ws" 'cofi/smart-split
-  "wS" 'split-window-below
-  "wv" 'split-window-right-and-focus
-  "w/" 'split-window-right-and-focus
-  "w-" 'split-window-below-and-focus
-  ;; "wa" 'ace-window
-  "wH" 'evil-window-move-far-left
-  "wh" 'evil-window-left
-  "wJ" 'evil-window-move-very-bottom
-  "wj" 'evil-window-down
-  "wK" 'evil-window-move-very-top
-  "wk" 'evil-window-up
-  "wL" 'evil-window-move-far-right
-  "wl" 'evil-window-right
-  "wu" 'winner-undo
-  "wU" 'winner-redo
-  "wo" 'other-window
-  "wf" 'other-frame
-  "wR" 'rotate-windows
+  "w C-l" 'swap-with-right
+  "w C-h" 'swap-with-left
+  "w C-j" 'swap-with-down
+  "w C-k" 'swap-with-up
 
-  "ff" 'helm-find-files
   "fS" 'save-some-buffers
 
   "nd" 'narrow-or-widen-dwim
 
-  "yn" 'yas-new-snippet
-  "yt" 'tiny-expand
-
-  "bk" 'kill-buffer
-
-  "ty" 'yas-minor-mode
-
   "om" 'op/copy-last-message
   "ow" 'whitespace-cleanup
-
+  "oh" 'mark-whole-buffer
+  "or" 'regexp-builder
+  "ox" 'luxbock/swap-command-key
   "oa"  'luxbock/switch-to-default-agenda-view
   "ot"  'org-capture
+  "wf"  'spacemacs/toggle-fullscreen
   "oci" 'bh/punch-in
   "ocs" 'bh/punch-out
   "oco" 'org-clock-out
   "ocg" 'org-clock-goto
   "occ" 'org-clock-cancel)
 
+
 (global-set-key (kbd "C-x C-k") 'kill-buffer)
+(global-set-key (kbd "C-j") 'newline-and-indent)
 
 (evil-define-key 'normal global-map "K" 'helm-show-kill-ring)
 
-;; (evil-leader/set-key "om" 'op/copy-last-message)
+(spacemacs/set-leader-keys "om" 'op/copy-last-message)
 
-;; Company-mode
-;; (define-key company-active-map (kbd "C-k") nil)
-;; (define-key company-active-map (kbd "C-j") 'newline-and-indent)
-;; (define-key company-active-map (kbd "M-j") 'company-select-next)
-;; (define-key company-active-map (kbd "M-k") 'company-select-previous)
-;; (define-key company-active-map (kbd "C-/") 'company-search-candidates)
-;; (define-key company-active-map (kbd "C-M-/") 'company-filter-candidates)
-;; (define-key company-active-map (kbd "C-d") 'company-show-doc-buffer)
-;; (define-key company-active-map (kbd "C-w") 'subword-backward-kill)
+(define-key evil-normal-state-map (kbd "M-DEL") 'evil-window-map)
+
+
+(evil-define-key 'insert global-map (kbd "C-e") 'move-end-of-line)
+(evil-define-key 'insert global-map (kbd "C-a") 'beginning-of-line)
